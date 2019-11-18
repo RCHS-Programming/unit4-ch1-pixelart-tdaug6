e939be58b662324b96b0613ccf24e16c6cc2b7d8
tdaug6 <57757881+tdaug6@users.noreply.github.com>
2019-11-18 08:13:22 -0500

    Added colors

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,10 @@
+var penColour = black;
+
+function setPenColour(pen){
+penColour = pen;
+}
+
 function setPixelColour(pixel)
 {
-pixel.style.backgroundColor= 'black';
+pixel.style.backgroundColor= penColour;
 }
